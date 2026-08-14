85ce2480f9314e00964f4106d04c46ceac22eff5
probablySomeUser <jndissing@fibermail.dk>
2026-08-14 09:44:10 +0200

    Created the database from python

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import sqlite3
 
+def main():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    query = """
+    CREATE TABLE IF NOT EXISTS tracking (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    year INTEGER,
+    month INTEGER,
+    day INTEGER,
+    amount INTEGER,
+    category TEXT,
+    store TEXT
+    );    
+    """
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+
+if __name__ == '__main__':
+    main()
