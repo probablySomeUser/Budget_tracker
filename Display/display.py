a03a02ea334ede82098bb2347d91fd106d4405ab
probablySomeUser <jndissing@fibermail.dk>
2026-08-14 13:54:15 +0200

    Added basic display functionality
    Added basic display functionality. Should be changed later

--- Display/display.py
+++ Display/display.py
@@ -1,1 +1,16 @@
+import sqlite3
+from pathlib import Path
 
+def display():
+    database_path = Path(__file__).resolve().parent.parent / "database.db"
+    connection = sqlite3.connect(database_path)
+    cursor = connection.cursor()
+    query = """
+    SELECT category, amount FROM expenses
+    """
+    outprint = cursor.execute(query)
+    a= outprint.fetchmany(2)
+    connection.close()
+    print(a)
+
+display()
