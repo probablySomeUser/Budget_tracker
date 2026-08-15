af82d505c826b1d5a78af07454d68e80d66c2923
probablySomeUser <jndissing@fibermail.dk>
2026-08-15 15:28:12 +0200

    Added multiple categories to entry

--- Entry/entry.py
+++ Entry/entry.py
@@ -1,6 +1,7 @@
 import sqlite3
 from pathlib import Path
 from datetime import date
+from sys import exit
 
 def get_time(unit,message):
     match unit:
@@ -25,9 +26,111 @@ def get_time(unit,message):
             print('That number is not accepted')
     return answer
 
+def get_price_of_category(amount,total):
+    sub_amount = 0
+    while True: #Gets price
+        while True:
+           try:
+               price = int(float(input('Price of item:\n'))*100)
+           except:
+               print('That is not a number')
+               continue
+           if 0<price:
+               break
+           else:
+               print('Not possible')
+        sub_amount += price
+        total += price
+        if total > amount:
+            print('You have spent more than you originally said??')
+            exit()
+        #Ask if category is done
+        while True:
+            answer = input('Is this the final item of the category?[y/n]')
+            match answer:
+                case 'y'|'n':
+                    break
+                case _:
+                    print('I did not understand that')
+                    continue
+        if answer == 'y':
+            return sub_amount,total
+
+def add_to_database(year,month,day,amount,category,store):
+    database_path = Path(__file__).resolve().parent.parent / "database.db"
+    connection = sqlite3.connect(database_path)
+    cursor = connection.cursor()
+    query = """
+        INSERT INTO expenses (year, month, day, amount, category, store)
+        VALUES (?,?,?,?,?,?)
+    """
+    cursor.execute(query,(year,month,day,amount,category,store))
+    connection.commit()
+    connection.close()
+
+def select_category(categories):
+    counter = 1
+    for i in categories:
+        print(counter, ') ',i)
+        counter += 1
+    while True: #Selects category
+        try:
+           answer = int(input())
+        except:
+            print('That is not a number')
+            continue
+        if 0<answer <= counter:
+            break
+        else:
+            print('That is not an option')
+    category = categories[answer-1]
+    return category
+
+def multiple_categories(amount,categories,year,month,day,store):
+    database_path = Path(__file__).resolve().parent.parent / "database.db"
+    connection = sqlite3.connect(database_path)
+    cursor = connection.cursor()
+    print('What is the first category? Your options are')
+    category = select_category(categories)
+    total = 0
+    sub_amount,total = get_price_of_category(amount,total)
+    query = """
+        INSERT INTO expenses (year, month, day, amount, category, store)
+        VALUES (?,?,?,?,?,?)
+        """
+    cursor.execute(query,(year,month,day,sub_amount,category,store))
+    while True: #Deals with the remaining categories
+        print('What is the next category?\n')
+        category = select_category(categories)
+        #Ask if is final category
+        while True:
+                answer = input('Is this the final category?[y/n]\n')
+                match answer:
+                    case 'y':
+                        query = """
+        INSERT INTO expenses (year, month, day, amount, category, store)
+        VALUES (?,?,?,?,?,?)
+    """
+                        cursor.execute(query,(year,month,day,amount-total,category,store))
+                        break
+                    case 'n':
+                        sub_amount,total = get_price_of_category(amount,total)
+                        query = """
+        INSERT INTO expenses (year, month, day, amount, category, store)
+        VALUES (?,?,?,?,?,?)
+        """
+                        cursor.execute(query,(year,month,day,sub_amount,category,store))
+                        break
+                    case _:
+                        print('I do not understand')
+        if answer == 'y':
+                    break
+    connection.commit()
+    connection.close()
+
+
 
 def enter():
-    database_path = Path(__file__).resolve().parent.parent / "database.db"
     print('You have chosen to enter a purchase. When was the purchase?')
     print('1) Today')
     print('2) This month, but not today')
@@ -57,8 +160,15 @@ def enter():
                 print("I don't believe that")
         except:
             print('That is not a number')
-    print('What is the cateory? Your options are')
+    while True:
+        store = input('Where did you buy it?\n')
+        if store != '':
+            break
+        else:
+            print('Please enter something')
+    print('What is the category? Your options are')
     categories = ['Rent','Insurance','Subscriptions','Internet','Food','Non-food','Transport','Fun','Other']
+    print('0) Multiple categories')
     counter = 1
     for i in categories:
         print(counter, ') ',i)
@@ -69,26 +179,17 @@ def enter():
         except:
             print('That is not a number')
             continue
-        if 0<answer <= counter:
+        if 0<=answer <= counter:
             break
         else:
             print('That is not an option')
-    category = categories[answer-1]
-    while True:
-        store = input('Where did you buy it?\n')
-        if store != '':
-            break
-        else:
-            print('Please enter something')
-    connection = sqlite3.connect(database_path)
-    cursor = connection.cursor()
-    query = """
-        INSERT INTO expenses (year, month, day, amount, category, store)
-        VALUES (?,?,?,?,?,?)
-    """
-    cursor.execute(query,(year,month,day,amount,category,store))
-    connection.commit()
-    connection.close()
+    if answer == 0:
+        multiple_categories(amount,categories,year,month,day,store)
+        return 0
+    else:
+        category = categories[answer-1]
+        add_to_database(year,month,day,amount,category,store)
+        return 0
 
 def process():
     enter()
